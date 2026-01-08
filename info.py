035508e20543d1db535caa6570fd3f5cc6d8e3fb
Lakki reddy <pulka143341@gmail.com>
2026-01-08 14:15:01 +0530

    Update APP_NAME initialization to None
    Change APP_NAME from autofilterpaid to None

--- info.py
+++ info.py
@@ -206,7 +206,7 @@ BAD_WORDS = {
 # ============================
 
 NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = autofilterpaid
+APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
